87bdd2d8443ca7410cd05116fe9b0c38ddf130df
jans-code <7jantb@gmx.de>
2023-09-20 09:51:02 +0200

    changed heads

--- src/janschatgptkernel/kernel.py
+++ src/janschatgptkernel/kernel.py
@@ -21,7 +21,7 @@ class janschatgptkernel(Kernel):
                    allow_stdin=False):
         if not silent:            
             completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
+                model="gpt-4",
                 messages=[
                     {"role": "user", "content": code}
                 ]
